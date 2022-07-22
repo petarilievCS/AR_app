4899a71b556bbfed0147c18c1ec3d4240c5b6e82
petar.iliev <petar.iliev@servicenow.com>
2022-07-22 14:36:49 -0700

    Added a red box into 3D space

--- AR Application/AR Application/ViewController.swift
+++ AR Application/AR Application/ViewController.swift
@@ -19,11 +19,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let cube = SCNBox(width: 0.15, height: 0.15, length: 0.15, chamferRadius: 0.01)
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        // change cube color to red
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        cube.materials = [material]
+        
+        // position box in 3D space
+        let node = SCNNode()
+        node.position = SCNVector3(0, 0.1, -0.5)
+        node.geometry = cube
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting = true
+        
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
