777effd50eae6b78ca1a68a74e4297dae1a62932
petar.iliev <petar.iliev@servicenow.com>
2022-07-29 12:58:30 -0700

    Implemented functionality to roll a dice when user places it into 3D space

--- AR Application/AR Application/ViewController.swift
+++ AR Application/AR Application/ViewController.swift
@@ -59,6 +59,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                         actualLocation.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
                         actualLocation.worldTransform.columns.3.z)
                     sceneView.scene.rootNode.addChildNode(diceNode)
+                    
+                    // create random rotation angles
+                    let randomX = Float((arc4random_uniform(4) + 1)) * (Float.pi / 2)
+                    let randomZ = Float((arc4random_uniform(4) + 1)) * (Float.pi / 2)
+                    
+                    diceNode.runAction(SCNAction.rotateBy(
+                        x: CGFloat(randomX) * 5,
+                        y: 0.0,
+                        z: CGFloat(randomZ) * 5,
+                        duration: 0.5))
                 }
             }
         }
